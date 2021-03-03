4e5e0253d12af9be1f8a83cbc45847a8d2e6abef
czak <banaczykczarek@gmail.com>
2021-03-03 13:13:30 +0100

    update

---
+++ Zadanie7/src/com/company/z1/Animal.java
@@ -0,0 +1,11 @@
+package com.company.z1;
+
+public class Animal {
+    protected String name;
+    protected String color;
+
+    public Animal(String name, String color){
+        this.name = name;
+        this.color = color;
+    }
+}
---
+++ Zadanie7/src/com/company/z1/main.java
@@ -0,0 +1,12 @@
+package com.company.z1;
+
+public class main {
+    public static void main(String[] args) {
+        Dog pies = new Dog("Reks", "Szary");
+        pies.makeSound();
+
+        Cat kot = new Cat("Puszek", "Czarny");
+        kot.makeSound();
+
+    }
+}
